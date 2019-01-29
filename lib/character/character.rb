88e8bb29a45bae32141ccc676c8042a844989711
RyanWolfen7 <wolfen7lada@gmail.com>
2019-01-29 17:40:05 +0000

    second test passed with hp, mgk, stm

--- lib/character/character.rb
+++ lib/character/character.rb
@@ -1,11 +1,15 @@
 class Character
+  DEFAULT_STATS = 100
 
-  attr_reader :name, :race, :class
+  attr_reader :name, :race, :class, :hp, :magic, :stamina
 
   def initialize(name, race, c_class)
     @name = name
     @race = race
     @class = c_class
+    @hp = DEFAULT_STATS
+    @magic = DEFAULT_STATS
+    @stamina = DEFAULT_STATS
   end
 
 end
