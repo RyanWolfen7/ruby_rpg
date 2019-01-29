b3be6ea160f9dbdc4045a6af075de7087a6d20b2
RyanWolfen7 <wolfen7lada@gmail.com>
2019-01-29 16:54:31 +0000

    made the initial character class

---
+++ spec/character/character_spec.rb
@@ -0,0 +1,13 @@
+require 'lib/character/character.rb'
+
+describe Character do
+  describe "#Init for Class" do
+    it 'should have a name, race, class' do
+      character = Character.new("Name", "Race", "Class")
+      expect(character.name).to eq("Name")
+      expect(character.race).to eq("Race")
+      expect(character.class).to eq("Class")
+    end
+  end
+
+end
