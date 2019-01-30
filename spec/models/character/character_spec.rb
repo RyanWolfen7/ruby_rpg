cefc186d5ae10d389ccac97f283d93fa74691e25
RyanWolfen7 <wolfen7lada@gmail.com>
2019-01-30 14:32:11 +0000

    changed health values in spec

--- spec/models/character/character_spec.rb
+++ spec/models/character/character_spec.rb
@@ -10,18 +10,18 @@ describe Character do
       expect(character.class).to eq("Class")
     end
 
-    it 'should have a default max Health, Magic, Stamina' do
+    it 'should have a default max Health, Magic, Stamina with default stats' do
       character = Character.new("Name", "Race", "Class")
-      expect(character.hp[:max]).to eq(100)
-      expect(character.magic[:max]).to eq(100)
-      expect(character.stamina[:max]).to eq(100)
+      expect(character.hp[:max]).to eq(36)
+      expect(character.magic[:max]).to eq(24)
+      expect(character.stamina[:max]).to eq(36)
     end
 
     it 'should have a current health, magic, stamina' do
       character = Character.new("Name", "Race", "Class")
-      expect(character.hp[:current]).to eq(100)
-      expect(character.magic[:current]).to eq(100)
-      expect(character.stamina[:current]).to eq(100)
+      expect(character.hp[:current]).to eq(36)
+      expect(character.magic[:current]).to eq(24)
+      expect(character.stamina[:current]).to eq(36)
     end
 
     it 'should have stats' do
@@ -71,36 +71,34 @@ describe Character do
     it 'should reach level 2 when max xp reached' do
       character = Character.new("Name", "Race", "Class")
       character.xp_gain(100)
-      character.level_up
       expect(character.level).to eq 2
     end
 
     it 'max xp should jump to 250 and current should reset to 0' do
       character = Character.new("Name", "Race", "Class")
-      character.level_up
       expect(character.xp[:max]).to eq 250
       expect(character.xp[:current]).to eq 0
     end
 
-    it 'should increase HP from 100 to 106 with default stats' do
+    it 'should increase HP from 36 to 42 with default stats' do
       character = Character.new("Name", "Race", "Class")
       character.level_up
-      expect(character.hp[:max]).to eq 106
-      expect(character.hp[:current]).to eq 106
+      expect(character.hp[:max]).to eq 42
+      expect(character.hp[:current]).to eq 42
     end
 
-    it 'should increase Magic from 100 to 108 with default stats' do
+    it 'should increase Magic from 24 to 32 with default stats' do
       character = Character.new("Name", "Race", "Class")
       character.level_up
-      expect(character.magic[:max]).to eq 108
-      expect(character.magic[:current]).to eq 108
+      expect(character.magic[:max]).to eq 32
+      expect(character.magic[:current]).to eq 32
     end
 
-    it 'should increase stamina from 100 to 106 with default stats' do
+    it 'should increase stamina from 36 to 42 with default stats' do
       character = Character.new("Name", "Race", "Class")
       character.level_up
-      expect(character.stamina[:max]).to eq 106
-      expect(character.stamina[:current]).to eq 106
+      expect(character.stamina[:max]).to eq 42
+      expect(character.stamina[:current]).to eq 42
     end
   end
 
