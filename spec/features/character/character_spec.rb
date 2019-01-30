c3601c3073affb799fb3213709abff15acf14742
RyanWolfen7 <wolfen7lada@gmail.com>
2019-01-30 20:00:36 +0000

    added feature test to add race bonus to stats

--- spec/features/character/character_spec.rb
+++ spec/features/character/character_spec.rb
@@ -13,6 +13,13 @@ describe '###Feature Test###' do
         expect(character.race.sub_race).to eq "Standard"
         expect(character.race.stat_bonus).to eq stat_bonus
       end
+
+      it 'should add the human race bonus to character' do
+        race = Human.new
+        character = Character.new("Ryan", race, "Ranger")
+        expect(character.stats[:char]).to eq 14
+        expect(character.stats[:int]).to eq 14
+      end
     end
   end
 end
