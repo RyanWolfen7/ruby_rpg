e3f07ce48265f6ac9f76890697194568b522e78f
RyanWolfen7 <wolfen7lada@gmail.com>
2019-01-30 07:25:56 +0000

    added xp gain to fix small bug in tests

--- spec/models/character/character_spec.rb
+++ spec/models/character/character_spec.rb
@@ -66,20 +66,27 @@ describe Character do
     end
   end
 
-  describe '#level up' do
+  describe '#level_up' do
     it 'should reach level 2 when max xp reached' do
       character = Character.new("Name", "Race", "Class")
-      character.xp[:current] = 100
+      character.xp_gain(100)
       character.level_up
       expect(character.level).to eq 2
     end
 
     it 'max xp should jump to 250 and current should reset to 0' do
       character = Character.new("Name", "Race", "Class")
-      character.xp[:current] = 100
       character.level_up
       expect(character.xp[:max]).to eq 250
       expect(character.xp[:current]).to eq 0
+    end
+  end
+
+  describe '#xp_gain' do
+    it 'should add xp to current' do
+      character = Character.new("Name", "Race", "Class")
+      character.xp_gain(50)
+      expect(character.xp[:current]).to eq 50
     end
   end
 
