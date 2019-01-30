57aa7d92e4c06babd8fad38382e802bf9131e4d8
RyanWolfen7 <wolfen7lada@gmail.com>
2019-01-30 19:44:58 +0000

    added a feature test for testing character chicago style

---
+++ spec/features/character/character_spec.rb
@@ -0,0 +1,18 @@
+require './lib/character/character.rb'
+require './lib/character/races/human.rb'
+
+describe '###Feature Test###' do
+  describe Character do
+    describe '#init' do
+      it 'should load a race and be able to be called' do
+        stat_bonus = { char: 2, int: 2}
+        race = Human.new
+        character = Character.new("Ryan", race, "Ranger")
+        expect(character.race).to eq race
+        expect(character.race.name).to eq "Human"
+        expect(character.race.sub_race).to eq "Standard"
+        expect(character.race.stat_bonus).to eq stat_bonus
+      end
+    end
+  end
+end
