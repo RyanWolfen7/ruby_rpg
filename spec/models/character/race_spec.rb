a081dba80f2398d49fee96307ec72d210be8d697
RyanWolfen7 <wolfen7lada@gmail.com>
2019-01-30 20:40:50 +0000

    added more tests for init on race

--- spec/models/character/race_spec.rb
+++ spec/models/character/race_spec.rb
@@ -6,5 +6,15 @@ describe Race do
       race = Race.new
       expect(race.name).to eq nil
     end
+
+    it 'should hold a nil sub_race' do
+      race = Race.new
+      expect(race.sub_race).to eq nil
+    end
+
+    it 'should hold a nil bonus' do
+      race = Race.new
+      expect(race.bonus).to eq nil
+    end
   end
 end
