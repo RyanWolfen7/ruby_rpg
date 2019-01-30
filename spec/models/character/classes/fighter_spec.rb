eb2213d39bc7578822a39bccf0f56192e62fbf59
RyanWolfen7 <wolfen7lada@gmail.com>
2019-01-30 21:46:15 +0000

    added test for fighter class

---
+++ spec/models/character/classes/fighter_spec.rb
@@ -0,0 +1,10 @@
+require './lib/classes/fighter.rb'
+
+describe Fighter do
+  describe '#initialize' do
+    it 'should be named fighter' do
+      fighter = Fighter.new
+      expect(fighter.name).to eq "Fighter"
+    end
+  end
+end
