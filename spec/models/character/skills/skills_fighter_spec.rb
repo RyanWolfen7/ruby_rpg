b8fe23bb8a4dc053e90c9caa048a7a87b855feea
RyanWolfen7 <wolfen7lada@gmail.com>
2019-01-31 19:11:15 +0000

    fixed some logic errors

--- spec/models/character/skills/skills_fighter_spec.rb
+++ spec/models/character/skills/skills_fighter_spec.rb
@@ -11,8 +11,9 @@ describe Skills_fighters do
   describe '#simple_first_aid' do
     it 'should return a random number between 3-5' do
       fighter = Skills_fighters.new
-      allow(fighter.simple_first_aid).to receive(:rand).and_return(3)
-      expect(fighter.simple_first_aid).to eq 3
+      target = {max: 100, current: 90}
+      allow(subject).to receive(:rand).and_return(3)
+      expect(fighter.simple_first_aid(target)).to eq 93
     end
   end
 end
