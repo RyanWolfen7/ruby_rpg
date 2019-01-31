049fe0329f2b32f1c1af62e5625df636cc233ed1
RyanWolfen7 <wolfen7lada@gmail.com>
2019-01-31 13:53:55 +0000

    added skills to init test

--- spec/models/character/classes/fighter_spec.rb
+++ spec/models/character/classes/fighter_spec.rb
@@ -12,5 +12,10 @@ describe Fighter do
       stats = { str: 1, const: 1, dex: 1}
       expect(fighter.stat_bonus).to eq stats
     end
+
+    it 'should have a skills list' do
+      fighter = Fighter.new('skills')
+      expect(fighter.skills).to eq 'skills'
+    end
   end
 end
