890c7aa152976c0fc4b2844796a34ab522875e58
RyanWolfen7 <wolfen7lada@gmail.com>
2019-01-31 14:27:09 +0000

    added skills fighter test and file

---
+++ lib/character/skills/skills_fighter.rb
@@ -0,0 +1,6 @@
+require_relative '../skills.rb'
+
+class Skills_fighters
+
+
+end
---
+++ spec/models/character/skills/skills_fighter_spec.rb
@@ -0,0 +1,9 @@
+require './lib/character/skills/skills_fighter.rb'
+
+describe Skills_fighters do
+  describe '#Initialize' do
+    it 'should have a name' do
+
+    end
+  end
+end
