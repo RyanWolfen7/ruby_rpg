dd14f9dc917aac1170d49b3d9b01b35057ba8771
RyanWolfen7 <wolfen7lada@gmail.com>
2019-01-31 13:57:37 +0000

    added skills to init

--- lib/character/classes/fighter.rb
+++ lib/character/classes/fighter.rb
@@ -1,10 +1,13 @@
 require_relative '../class.rb'
 
 class Fighter < C_class
-  attr_reader :name, :stat_bonus
+  TEST_SKILLS = [0,1,2,3,4,5]
 
-  def initialize
+  attr_reader :name, :stat_bonus, :skills
+
+  def initialize(skills = TEST_SKILLS)
     @name = "Fighter"
     @stat_bonus = { str: 1, const: 1, dex: 1}
+    @skills = skills
   end
 end
