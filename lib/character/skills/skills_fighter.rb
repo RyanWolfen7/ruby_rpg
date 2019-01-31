ca9e00d1ca4da13787b21d18201eb2bf21f422d0
RyanWolfen7 <wolfen7lada@gmail.com>
2019-01-31 15:06:52 +0000

    initiated skills fighter

--- lib/character/skills/skills_fighter.rb
+++ lib/character/skills/skills_fighter.rb
@@ -1,6 +1,11 @@
 require_relative '../skills.rb'
 
-class Skills_fighters
+class Skills_fighters < Skills
 
+  attr_reader :list
+
+    def initialize
+      @list = []
+    end
 
 end
--- spec/models/character/skills/skills_fighter_spec.rb
+++ spec/models/character/skills/skills_fighter_spec.rb
@@ -3,7 +3,8 @@ require './lib/character/skills/skills_fighter.rb'
 describe Skills_fighters do
   describe '#Initialize' do
     it 'should have a name' do
-
+      fighter = Skills_fighters.new
+      expect(fighter.list).to eq []
     end
   end
 end
