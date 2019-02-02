ca17282524a1163154f73dab8a6e6882b720e233
RyanWolfen7 <wolfen7lada@gmail.com>
2019-02-02 18:06:15 +0000

    added type to weapon

--- lib/character/weapon.rb
+++ lib/character/weapon.rb
@@ -1,10 +1,11 @@
 class Weapon
 
-  attr_reader :name, :reach, :unique_name
+  attr_reader :name, :reach, :unique_name, :type
 
   def initialize
     @name = nil
     @reach = nil
     @unique_name = nil
+    @type = nil
   end
 end
