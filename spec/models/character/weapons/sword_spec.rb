8ab7150fc20ad05dc6c50d9d31d29da2570bab31
RyanWolfen7 <wolfen7lada@gmail.com>
2019-02-02 17:43:19 +0000

    added reach inheritence

--- spec/models/character/weapons/sword_spec.rb
+++ spec/models/character/weapons/sword_spec.rb
@@ -8,7 +8,11 @@ describe Sword do
   describe '#initialize' do
     it 'should inherit name' do
       sword = Sword.new
-      expect(sword.name). to eq nil
+      expect(sword.name).to eq nil
+    end
+    it 'should inherit reach' do
+      sword = Sword.new
+      expect(sword.reach).to eq nil
     end
     it 'should have a handed' do
       sword = Sword.new
