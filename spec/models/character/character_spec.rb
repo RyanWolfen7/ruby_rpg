610257908c87d08955a4f8032d7ab2c0a84ed852
RyanWolfen7 <wolfen7lada@gmail.com>
2019-02-02 17:06:37 +0000

    added option for weapon on attack test

--- spec/models/character/character_spec.rb
+++ spec/models/character/character_spec.rb
@@ -123,6 +123,14 @@ describe Character do
       character.attack(enemy)
       expect(enemy[:current]).to eq 95
     end
+
+    it 'should deal damage with weapon modifier' do
+      character = Character.new("Name", "Race", "Class")
+      enemy = {max: 100, current:100}
+      allow(character).to receive(:rand).and_return(5)
+      character.attack(enemy, 5)
+      expect(enemy[:current]).to eq 90
+    end
   end
 
 end
