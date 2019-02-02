7c9d498b7b3d46a8d08e955835051abd5bcd44c8
RyanWolfen7 <wolfen7lada@gmail.com>
2019-02-02 17:14:07 +0000

    fixed spec format

--- spec/models/character/weapon_spec.rb
+++ spec/models/character/weapon_spec.rb
@@ -1,8 +1,10 @@
 require './lib/character/weapon.rb'
 
 describe Weapon do
-  describe '#initialize do' do
-    weapon = Weapon.new
-    expect(weapon.name).to be nil
+  describe '#initialize' do
+    it 'should hold a nil name' do
+      weapon = Weapon.new
+      expect(weapon.name).to eq nil
+    end
   end
 end
