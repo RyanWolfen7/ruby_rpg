16cbb3cf7c912da2d8a9d77bb194646719ce9650
RyanWolfen7 <wolfen7lada@gmail.com>
2019-02-02 17:38:31 +0000

    added type and checking if it is inherited

---
+++ spec/models/character/weapons/sword_spec.rb
@@ -0,0 +1,23 @@
+require './lib/character/weapons/sword.rb'
+
+describe Sword do
+  describe Sword do
+    it { expect(described_class).to be < Weapon }
+  end
+
+  describe '#initialize' do
+    it 'should inherit name' do
+      sword = Sword.new
+      expect(sword.name). to eq nil
+    end
+    it 'should have a handed' do
+      sword = Sword.new
+      expect(sword.handed).to eq nil
+    end
+
+    it 'should have a type' do
+      sword = Sword.new
+      expect(sword.type).to eq nil
+    end
+  end
+end
