240c7a911aace2a892c39ad23f1084915af8b3dd
RyanWolfen7 <wolfen7lada@gmail.com>
2019-02-04 09:40:01 +0000

    fixed logic error on dead spec

--- spec/models/core/dead_spec.rb
+++ spec/models/core/dead_spec.rb
@@ -1,15 +1,17 @@
-require 'lib/core/dead?.rb'
+require './lib/core/dead.rb'
 
-describe Dead? do
+describe Dead do
   describe '#initialize' do
     it 'should return false' do
       hp = {max: 100, current: 1}
-      expect(Dead?(hp)).to eq false
+      dead = Dead.new
+      expect(dead.true?(hp)).to eq false
     end
 
     it 'should return true' do
       hp = {max: 100, current: 0}
-      expect(Dead?(hp)).to eq true
+      dead = Dead.new
+      expect(dead.true?(hp)).to eq true
     end
   end
 end
