a604845912886ca51c224d813025de65093affc2
RyanWolfen7 <wolfen7lada@gmail.com>
2019-02-04 10:23:43 +0000

    fixed logic errors for refactor

--- spec/models/core/attack_spec.rb
+++ spec/models/core/attack_spec.rb
@@ -4,18 +4,18 @@ describe Attack  do
 
   describe '#damage' do
     it 'should deal damage' do
-      character = Character.new("Name", "Race", "Class")
+      attack = Attack.new
       enemy = {max: 100, current:100}
-      allow(character).to receive(:rand).and_return(5)
-      character.attack(enemy)
+      allow(attack).to receive(:rand).and_return(5)
+      attack.damage(enemy)
       expect(enemy[:current]).to eq 95
     end
 
     it 'should deal damage with weapon modifier' do
-      character = Character.new("Name", "Race", "Class")
+      attack = Attack.new
       enemy = {max: 100, current:100}
-      allow(character).to receive(:rand).and_return(5)
-      character.attack(enemy, 5)
+      allow(attack).to receive(:rand).and_return(5)
+      attack.damage(enemy)
       expect(enemy[:current]).to eq 90
     end
   end
