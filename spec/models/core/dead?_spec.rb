95cef56ec956ef0c5fa2f9a3074a3cc405558378
RyanWolfen7 <wolfen7lada@gmail.com>
2019-02-04 09:31:07 +0000

    fixed pointer error

--- spec/models/core/dead?_spec.rb
+++ spec/models/core/dead?_spec.rb
@@ -3,12 +3,12 @@ require 'lib/core/dead?.rb'
 describe Dead? do
   describe '#initialize' do
     it 'should return false' do
-      hp = {:max 100, current: 1}
+      hp = {max: 100, current: 1}
       expect(Dead?(hp)).to eq false
     end
 
     it 'should return true' do
-      hp = {:max 100, current: 0}
+      hp = {max: 100, current: 0}
       expect(Dead?(hp)).to eq true
     end
   end
