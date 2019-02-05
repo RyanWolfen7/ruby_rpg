f190e887fe0860fe4e3da03b602dad92764d77aa
RyanWolfen7 <wolfen7lada@gmail.com>
2019-02-05 16:57:38 +0000

    finished greatsword damage

--- lib/character/weapons/swords/great_sword.rb
+++ lib/character/weapons/swords/great_sword.rb
@@ -1,4 +1,15 @@
 class Great_sword < Sword
+  def initialize(unique_name = nil)
+    @name = 'Great Sword'
+    @unique_name = unique_name
+    @reach = 8
+    @handed = 2
+    @switchable = false
+    @type = 'sword'
+  end
 
+  def damage
+    rand(1..20) + rand(1..20)
+  end
 
 end
