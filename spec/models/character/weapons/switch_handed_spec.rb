a237bc6f72b2151b958e82f5f8bfa46585a485c4
RyanWolfen7 <wolfen7lada@gmail.com>
2019-02-06 13:25:48 +0000

    changed how the test works and how switch_handed works

--- spec/models/character/weapons/switch_handed_spec.rb
+++ spec/models/character/weapons/switch_handed_spec.rb
@@ -1,23 +1,62 @@
 require './lib/character/weapons/switch_handed.rb'
 
 describe Switch_handed do
-  let(:switch_handed) {Switch_handed.new}
+  class Double_switchable
+    attr_accessor :switchable, :handed
+
+    def initialize
+      @handed = 2
+      @switchable = true
+    end
+  end
+
+  class Double_not_switchable
+    attr_accessor :switchable, :handed
+
+    def initialize
+      @handed = 2
+      @switchable = false
+    end
+  end
+
+  class Double_single
+    attr_accessor :switchable, :handed
+
+    def initialize
+      @handed = 1
+      @switchable = false
+    end
+  end
+
+  let(:double_switchable) {Double_switchable.new}
+  let(:double_not_switchable) {Double_not_switchable.new}
+  let(:double_single) {Double_single.new}
+
 
   describe '#switch' do
     it 'should switch to single handed' do
-      expect(switch_handed.switch(true, 2)).to eq 1
+      switch = Switch_handed.new(double_switchable)
+      switch.switch
+      expect(double_switchable.handed).to eq 1
     end
 
     it 'should switch back to two handed' do
-      expect(switch_handed.switch(true, 1)).to eq 2
+      switch = Switch_handed.new(double_switchable)
+      switch.switch
+      switch.switch
+      expect(double_switchable.handed).to eq 2
     end
 
     it 'should not change from one handed' do
-      expect(switch_handed.switch(false, 1)).to eq 1
+      switch = Switch_handed.new(double_single)
+      switch.switch
+      expect(double_single.handed).to eq 1
     end
 
     it 'should not change from two haned' do
-      expect(switch_handed.switch(false, 2)).to eq 2
+      switch = Switch_handed.new(double_not_switchable)
+      switch.switch
+      expect(double_not_switchable.handed).to eq 2
     end
   end
 end
