389f94a5a8b2ab4855773a2cc044275b9266bee1
RyanWolfen7 <wolfen7lada@gmail.com>
2019-02-06 13:53:13 +0000

    deleted redundant code

--- lib/character/classes/fighter.rb
+++ lib/character/classes/fighter.rb
@@ -1,11 +1,10 @@
 require_relative '../class.rb'
 
 class Fighter < C_class
-  TEST_SKILLS = [0,1,2,3,4,5]
 
   attr_reader :name, :stat_bonus, :skills
 
-  def initialize(skills = TEST_SKILLS)
+  def initialize(skills)
     @name = "Fighter"
     @stat_bonus = { str: 1, const: 1, dex: 1}
     @skills = skills
