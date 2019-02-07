427e9d3844f062838ef75774c5a748a1d6a25913
RyanWolfen7 <wolfen7lada@gmail.com>
2019-02-07 19:49:27 +0000

    added expect for race arch class and fixed stats let

--- spec/models/character/races/elf_spec.rb
+++ spec/models/character/races/elf_spec.rb
@@ -1,8 +1,12 @@
 require './lib/character/races/elf.rb'
 
 describe Elf do
+
+  it { expect(described_class).to be < Race }
+
   let(:elf) {Elf.new}
-  let(:stats) {{wis: 2}}
+  let(:stats) {{wis: 2, int: 2}}
+
 
   describe '#initialize' do
     it 'has name of elf' do
