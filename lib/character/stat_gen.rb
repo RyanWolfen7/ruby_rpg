77a6bc9bb551860d0a5f9707c157b23e7f4b692d
RyanWolfen7 <wolfen7lada@gmail.com>
2019-02-08 18:58:01 +0000

    fixed points cal

--- lib/character/stat_gen.rb
+++ lib/character/stat_gen.rb
@@ -1,8 +1,27 @@
 class Stat_gen
-  attr_reader :stats, :points
+
+  DEFAULT_STATS = {
+    str: 12, dex: 12, const: 12,
+    char: 12, wis: 12, int: 12
+  }
+
+  attr_accessor :stats, :points
 
   def initialize
-    @stats = {}
+    @stats = DEFAULT_STATS
+    @points = Integer
+    points_cal
+  end
+
+  def points_cal
     @points = rand(24..38)
+  end
+
+  def add(stat)
+    temp = Hash.new(0)
+    stats.each {|key, count| temp[key] += count}
+    stat.each  {|key, count| temp[key] += count}
+    @stats = temp
+    @points -= 1
   end
 end
--- spec/models/character/stat_gen_spec.rb
+++ spec/models/character/stat_gen_spec.rb
@@ -4,18 +4,35 @@ describe Stat_gen do
   let(:gen) {Stat_gen.new}
 
   describe '#initialize' do
-    it 'should have an stat array with 0s' do
+    it 'should have an stat hash' do
       expect(gen.stats).to be_a Hash
+    end
+
+    it 'should have an stat array with base stats' do
+      default = {
+          str: 12, dex: 12, const: 12,
+          char: 12, wis: 12, int: 12
+      }
+      expect(gen.stats).to eq default
     end
 
     it 'should respond to min rand' do
       allow(gen).to receive(:rand).and_return(24)
-      expect(gen.points).to eq 24
+      expect(gen.points_cal).to eq 24
     end
 
     it 'should respond to min rand' do
-      allow(gen).to receive(:rand).and_return(34)
-      expect(gen.points).to eq 38
+      allow(gen).to receive(:rand).and_return(38)
+      expect(gen.points_cal).to eq 38
+    end
+  end
+
+  describe '#add' do
+    it 'should add 1 to strength and minues one point' do
+      allow(gen).to receive(:rand).and_return(24)
+      gen.add({str: 1})
+      expect(gen.stats[:str]).to eq 13
+      expect(gen.points).to eq 23
     end
   end
 end
