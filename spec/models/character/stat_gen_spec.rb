1a6b58a998a11d59e5d871e0d9937e0d151bdc21
RyanWolfen7 <wolfen7lada@gmail.com>
2019-02-08 17:54:28 +0000

    added stat gen rand test

--- spec/models/character/stat_gen_spec.rb
+++ spec/models/character/stat_gen_spec.rb
@@ -7,5 +7,15 @@ describe Stat_gen do
     it 'should have an stat array with 0s' do
       expect(gen.stats).to be_a Hash
     end
+
+    it 'should respond to min rand' do
+      allow(gen).to receive(:rand).and_return(24)
+      expect(gen.stat_gen).to eq 24
+    end
+
+    it 'should respond to min rand' do
+      allow(gen).to receive(:rand).and_return(34)
+      expect(gen.stat_gen).to eq 38
+    end
   end
 end
