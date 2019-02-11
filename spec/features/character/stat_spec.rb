23f1dfab533da647667b70783b3a0d6c28e1c319
RyanWolfen7 <wolfen7lada@gmail.com>
2019-02-11 10:01:01 +0000

    added stat gen feature test

--- spec/features/character/stat_spec.rb
+++ spec/features/character/stat_spec.rb
@@ -14,14 +14,37 @@ describe '###Feature Test###' do
       expect(stats.points).to eq 26
       4.times { stats.add({dex: 1})}
       4.times { stats.add({const: 1})}
-      4.times { stats.add({char: 1})}
-      4.times { stats.add({wis: 1})}
+      6.times { stats.add({char: 1})}
+      6.times { stats.add({wis: 1})}
       6.times { stats.add({int: 1})}
       expect(stats.stats[:dex]).to eq 16
       expect(stats.stats[:const]).to eq 16
-      expect(stats.stats[:char]).to eq 16
-      expect(stats.stats[:wis]).to eq 16
+      expect(stats.stats[:char]).to eq 18
+      expect(stats.stats[:wis]).to eq 18
       expect(stats.stats[:int]).to eq 18
+      expect(stats.points).to eq 0
+    end
+
+    it 'should load a character with the stats' do
+      allow(Kernel).to receive(:rand).and_return(24)
+      2.times { stats.subtract({str: 1})}
+      4.times { stats.add({dex: 1})}
+      4.times { stats.add({const: 1})}
+      6.times { stats.add({char: 1})}
+      6.times { stats.add({wis: 1})}
+      6.times { stats.add({int: 1})}
+      character = Character.new(
+        "Ryan", Human.new, Fighter.new(Skills_fighters), stats.stats
+      ) #human adds +1 to int and char, fighter adds +1 to str, dex, const
+      expect(character.stats[:str]).to eq 11
+      expect(character.stats[:dex]).to eq 17
+      expect(character.stats[:const]).to eq 17
+      expect(character.stats[:char]).to eq 20
+      expect(character.stats[:wis]).to eq 18
+      expect(character.stats[:int]).to eq 20
+      expect(character.hp[:max]).to eq 51
+      expect(character.magic[:max]).to eq 38
+      expect(character.stamina[:max]).to eq 51
     end
   end
 
